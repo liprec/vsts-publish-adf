65d45320685cd5ce90c5f7abf07540095709217e
Jan Pieter Posthuma <mail@jppp.org>
2017-06-19 17:28:51 +0200

    Update appveyor CI files

--- tests/appveyor.pester.ps1
+++ tests/appveyor.pester.ps1
@@ -3,71 +3,77 @@
 # We serialize XML results and pull them in appveyor.yml
 
 #If Finalize is specified, we collect XML output, upload tests, and indicate build errors
-param([switch]$Finalize)
+param(
+    [switch]$Finalize,
+    [switch]$Test,
+    [string]$ProjectRoot = $ENV:APPVEYOR_BUILD_FOLDER
+)
 
 #Initialize some variables, move to the project root
-$PSVersion = $PSVersionTable.PSVersion.Major
-$TestFile = "TestResultsPS$PSVersion.xml"
-$ProjectRoot = $ENV:APPVEYOR_BUILD_FOLDER
-Set-Location $ProjectRoot
+    $Timestamp = Get-date -uformat "%Y%m%d-%H%M%S"
+    $PSVersion = $PSVersionTable.PSVersion.Major
+    $TestFile = "TestResults_PS$PSVersion`_$TimeStamp.xml"
+
+    $Address = "https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)"
+    Set-Location $ProjectRoot
+
+    $Verbose = @{}
+    if($env:APPVEYOR_REPO_BRANCH -and $env:APPVEYOR_REPO_BRANCH -notlike "master")
+    {
+        $Verbose.add("Verbose",$True)
+    }
    
-
-#Run a test with the current version of PowerShell
-#Make things faster by removing most output
-if (-not $Finalize) {
-    "`n`tSTATUS: Testing with PowerShell $PSVersion`n"
-		
-    Import-Module Pester
-    Set-Variable ProgressPreference -Value SilentlyContinue
-    Invoke-Pester -Quiet -Path "$ProjectRoot\Tests" -OutputFormat NUnitXml -OutputFile "$ProjectRoot\$TestFile" -PassThru |
-        Export-Clixml -Path "$ProjectRoot\PesterResults$PSVersion.xml"
-}
-
-#If finalize is specified, check for failures and 
-else {
-    #Show status...
-    $AllFiles = Get-ChildItem -Path $ProjectRoot\*Results*.xml | Select -ExpandProperty FullName
-    "`n`tSTATUS: Finalizing results`n"
-    "COLLATING FILES:`n$($AllFiles | Out-String)"
-
-    #Upload results for test page
-    Get-ChildItem -Path "$ProjectRoot\TestResultsPS*.xml" | Foreach-Object {
-        
-        $Address = "https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)"
-        $Source = $_.FullName
-
-        "UPLOADING FILES: $Address $Source"
-
-        (New-Object 'System.Net.WebClient').UploadFile( $Address, $Source )
-    }
-
-    #What failed?
-    $Results = @( Get-ChildItem -Path "$ProjectRoot\PesterResults*.xml" | Import-Clixml )
-            
-    $FailedCount = $Results |
-        Select -ExpandProperty FailedCount |
-        Measure-Object -Sum |
-        Select -ExpandProperty Sum
+#Run a test with the current version of PowerShell, upload results
+    if($Test)
+    {
+        "`n`tSTATUS: Testing with PowerShell $PSVersion`n"
     
-    if ($FailedCount -gt 0) {
+        Import-Module Pester
 
-        $FailedItems = $Results |
-            Select -ExpandProperty TestResult |
-            Where {$_.Passed -notlike $True}
-
-        "FAILED TESTS SUMMARY:`n"
-        $FailedItems | ForEach-Object {
-            $Test = $_
-            [pscustomobject]@{
-                Describe = $Test.Describe
-                Context  = $Test.Context
-                Name     = "It $($Test.Name)"
-                Result   = $Test.Result
-            }
-        } |
-            Sort Describe, Context, Name, Result |
-            Format-List
-
-        throw "$FailedCount tests failed."
+        Invoke-Pester @Verbose -Path "$ProjectRoot\Tests" -OutputFormat NUnitXml -OutputFile "$ProjectRoot\$TestFile" -PassThru |
+            Export-Clixml -Path "$ProjectRoot\PesterResults_PS$PSVersion`_$Timestamp.xml"
+        
+        If($env:APPVEYOR_JOB_ID)
+        {
+            (New-Object 'System.Net.WebClient').UploadFile( $Address, "$ProjectRoot\$TestFile" )
+        }
     }
-}
+
+#If finalize is specified, display errors and fail build if we ran into any
+    If($Finalize)
+    {
+        #Show status...
+            $AllFiles = Get-ChildItem -Path $ProjectRoot\PesterResults*.xml | Select -ExpandProperty FullName
+            "`n`tSTATUS: Finalizing results`n"
+            "COLLATING FILES:`n$($AllFiles | Out-String)"
+
+        #What failed?
+            $Results = @( Get-ChildItem -Path "$ProjectRoot\PesterResults_PS*.xml" | Import-Clixml )
+            
+            $FailedCount = $Results |
+                Select -ExpandProperty FailedCount |
+                Measure-Object -Sum |
+                Select -ExpandProperty Sum
+    
+            if ($FailedCount -gt 0) {
+
+                $FailedItems = $Results |
+                    Select -ExpandProperty TestResult |
+                    Where {$_.Passed -notlike $True}
+
+                "FAILED TESTS SUMMARY:`n"
+                $FailedItems | ForEach-Object {
+                    $Item = $_
+                    [pscustomobject]@{
+                        Describe = $Item.Describe
+                        Context = $Item.Context
+                        Name = "It $($Item.Name)"
+                        Result = $Item.Result
+                    }
+                } |
+                    Sort Describe, Context, Name, Result |
+                    Format-List
+
+                throw "$FailedCount tests failed."
+            }
+    }
