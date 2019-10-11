8a0e8a66a25e60f95712d37043b608807e379194
Jan Pieter Posthuma <mail@jppp.org>
2019-10-11 10:55:56 +0200

    npm outdated + security fix

--- trigger-adf-pipeline/v2/triggeradfpipeline.ts
+++ trigger-adf-pipeline/v2/triggeradfpipeline.ts
@@ -26,11 +26,11 @@
  *  THE SOFTWARE.
  */
 
-import Q = require('q');
-import throat = require('throat');
-import task = require('vsts-task-lib/task');
-import path = require('path');
-import msRestAzure = require('ms-rest-azure');
+import Q from 'q';
+import throat from 'throat';
+import * as task from 'vsts-task-lib/task';
+import * as path from 'path';
+import * as msRestAzure from 'ms-rest-azure';
 import { TaskParameters } from './models/taskParameters';
 import { AzureModels } from './models/azureModels';
 
